6f20123aa70f289138a2b8f2e9dbd8ed3e0421e3
JingruiZhang <ruilovechu@live.com>
2020-09-26 11:09:02 +0800

    1. add ThirdParty including openctm resources. 2. create MyActor BP file. 3. modify Build.cs to use openctm library. 4. modify Actor.cpp to use test if openctm load success.

--- Source/BIMUnreal/BIMUnreal.Build.cs
+++ Source/BIMUnreal/BIMUnreal.Build.cs
@@ -1,6 +1,8 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System;
+using System.IO;
 
 public class BIMUnreal : ModuleRules
 {
@@ -12,12 +14,51 @@ public class BIMUnreal : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+        // 将 include 添加到 PublicIncludePaths
+        // PublicAdditionalLibraries 可以添加多个
+        // --------------------------------------
+        PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "include"));
+
+        // dll 的加载方式
+        // 暂且认为两个都要加吧
+        // --------------------
+        string dllPath_LabTest_dll = Path.Combine(ThirdPartyPath, "lib", "openctm.dll");
+        PublicDelayLoadDLLs.Add(dllPath_LabTest_dll);
+        RuntimeDependencies.Add(new RuntimeDependency(dllPath_LabTest_dll));
+    }
+
+    /// <summary>
+    /// ThirdParty 文件夹的所在路径
+    /// </summary>
+    public string ThirdPartyPath
+    {
+        get
+        {
+            // 这个地址是多少？
+            // ---------------
+            string notFullPath = Path.Combine(ModuleDirectory, "../../ThirdParty");
+
+            // 这里的调试方法
+            // 编译工程，从生成窗口查看
+            // C:\Users\zhang\Documents\Unreal Projects\UELabTest\Source\UELabTest\../../ThirdParty
+            // ------------------------
+            Console.WriteLine(String.Format("notFullPath is {0}", notFullPath));
+
+            // 调用 Path.GetFullPath
+            // 这个方法是把../../计算出来
+            // --------------------------
+            string fullPath = Path.GetFullPath(notFullPath);
+            Console.WriteLine(String.Format("fullPath is {0}", fullPath));
+
+            return fullPath;
+        }
+    }
 }
--- Source/BIMUnreal/Tests/MyActor.cpp
+++ Source/BIMUnreal/Tests/MyActor.cpp
@@ -2,12 +2,13 @@
 
 
 #include "MyActor.h"
+#include "Misc/Paths.h"
 
 // Sets default values
 AMyActor::AMyActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	//PrimaryActorTick.bCanEverTick = true;
 
 }
 
@@ -15,13 +16,26 @@ AMyActor::AMyActor()
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// openCTM dll “˝»Î≤‚ ‘
+	// --------------------
+	FString gamePath = FPaths::ConvertRelativePathToFull(FPaths::GameDir());
+	FString dllpath = gamePath + "/ThirdParty/lib/openctm.dll";
+	void * openctmDllHandle = FPlatformProcess::GetDllHandle(*dllpath);
+	if (openctmDllHandle)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("openctmDllHandle is not null"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("openctmDllHandle is null"));
+	}
 }
 
-// Called every frame
-void AMyActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
+//// Called every frame
+//void AMyActor::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
 
